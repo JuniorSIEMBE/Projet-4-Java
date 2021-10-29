066f00ff37af3b435ae7855379a211c088adb29c
SIEMBE Junior <siembejunior@gmail.com>
2021-10-29 02:44:06 -0800

    First version

---
+++ src/org/sid/ExBouton.java
@@ -0,0 +1,50 @@
+package org.sid;
+
+import java.awt.BorderLayout;
+import java.awt.Dialog;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class ExBouton extends JFrame {
+	
+	public ExBouton() {
+		//Appel du constructeur de la classe mère
+		super();
+		//créeation du label
+		JLabel label = new JLabel("Exemple de bouton");
+		//Création du bouton
+		JButton bouton = new JButton(" Quitter ");
+		//Création du panel avec comme disposition BorderLayout
+		JPanel panel = new JPanel(new BorderLayout());
+		//Ajout du label au panel au nord
+		panel.add(label, BorderLayout.NORTH);
+		//Ajout du bouton au panel au dud
+		panel.add(bouton, BorderLayout.SOUTH);
+		//Gestion de l'evênement
+		bouton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("vous avez cliqué!");
+			}
+		});
+		//modification du panel de la fenêtre
+		this.setContentPane(panel);
+		//Redéfinition de la taille de la fenêtre
+		this.pack();
+	}
+	
+
+	public static void main(String[] args) {
+		//Création d'un objet de la classe
+		ExBouton exemple = new ExBouton();
+		//Affichage de la fenêtre
+		exemple.setVisible(true);
+	}
+
+}
